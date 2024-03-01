b0d412887d8ad292fdc2f5a4b8c3fa3e285a986c
Anton Belski <anton.belski@gmail.com>
2024-03-01 18:15:15 +0300

    feat: add new commits popup

---
+++ src/components/modal-write/modal.jsx
@@ -0,0 +1,23 @@
+import { Button, Input, Modal, Rate } from 'antd';
+const { TextArea } = Input;
+import './modal.scss';
+const ModalWrite = ({ isOpen, onOk, centered, onCancel }) => {
+    return (
+        <Modal
+            title='Ваш отзыв'
+            open={isOpen}
+            onOk={onOk}
+            centered={centered}
+            onCancel={onCancel}
+            footer={[
+                <Button key='submit' type='primary' onClick={onOk}>
+                    Опубликовать
+                </Button>,
+            ]}
+        >    <Rate disabled />
+            <TextArea />
+        </Modal>
+    );
+};
+
+export default ModalWrite;
--- src/pages/feed-backs/feed-backs.tsx
+++ src/pages/feed-backs/feed-backs.tsx
@@ -5,6 +5,8 @@ import './feed-backs.scss';
 
 import { UserOutlined } from '@ant-design/icons';
 import { useGetReviewsQuery } from '../../api/auth-api';
+import { useState } from 'react';
+import ModalWrite from '@components/modal-write/modal';
 const routes = [
     {
         path: 'main',
@@ -17,50 +19,70 @@ const routes = [
 
 const FeedBacks = () => {
     const { data: reviews } = useGetReviewsQuery();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    }
+    
 
     return (
-            <div className='feedbacks-content'>
-                <header>
-                    <Breadcrumbs items={routes} />
-                </header>
-                <div className='wrapper-feedbacks-conten'>
-                    <div className='wrapper-commit'>
-                        {!!reviews?.length &&
-                            reviews.slice(-4).map((el) => (
-                                <div className='wrapper-card-commit' key={el.id}>
-                                    <div className='description-user'>
-                                        <div>
-                                            <Avatar
-                                                size={42}
-                                                src={el.imageSrc || ''}
-                                                icon={!el.imageSrc ? <UserOutlined /> : undefined}
-                                            />
-                                        </div>
-                                        <div className='name-user'>
-                                            {el.fullName || 'Пользователь'}
-                                        </div>
+        <div className='feedbacks-content'>
+            <header>
+                <Breadcrumbs items={routes} />
+            </header>
+            <div className='wrapper-feedbacks-conten'>
+                <div className='wrapper-commit'>
+                    {!!reviews?.length &&
+                        reviews.slice(-4).map((el) => (
+                            <div className='wrapper-card-commit' key={el.id}>
+                                <div className='description-user'>
+                                    <div>
+                                        <Avatar
+                                            size={42}
+                                            src={el.imageSrc || ''}
+                                            icon={!el.imageSrc ? <UserOutlined /> : undefined}
+                                        />
                                     </div>
-                                    <div className='wrapper-rate-text'>
-                                        <div className='wrapper-rate-date'>
-                                            <Rate disabled value={el.rating} />
-                                            <div className='date-commit'>
-                                                {new Date(el.createdAt).toLocaleDateString()}
-                                            </div>
-                                        </div>
-                                        <p>{el.message || ''}</p>
-                                    </div>
+                                    <div className='name-user'>{el.fullName || 'Пользователь'}</div>
                                 </div>
-                            ))}
-                    </div>
-                    <div className='wrapper-commit-btn'>
-                        <Button type='text' className='write-review-btn'>
-                            Написать отзыв
-                        </Button>
-                        <Button type='text'>Развернуть все отзывы</Button>
-                    </div>
+                                <div className='wrapper-rate-text'>
+                                    <div className='wrapper-rate-date'>
+                                        <Rate disabled value={el.rating} />
+                                        <div className='date-commit'>
+                                            {new Date(el.createdAt).toLocaleDateString()}
+                                        </div>
+                                    </div>
+                                    <p>{el.message || ''}</p>
+                                </div>
+                            </div>
+                        ))}
+                </div>
+                <div className='wrapper-commit-btn'>
+                    <Button type='text' className='write-review-btn' onClick={showModal}>
+                        Написать отзыв
+                    </Button>
+                    <Button type='text' className='expand-reviews-btn'>
+                        Развернуть все отзывы
+                    </Button>
                 </div>
             </div>
+            <ModalWrite 
+            isOpen={isModalOpen} 
+            onOk={handleOk} 
+            onCancel={handleCancel} 
+            centered={true}
+             />
+        </div>
     );
 };
+
 
 export default FeedBacks;
