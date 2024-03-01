2bfe0ca4b8f369240ecfbfe52a1f8ec487007847
Anton Belski <anton.belski@gmail.com>
2024-03-01 12:08:15 +0300

    feat: add card commits

--- src/pages/base-page/base-page.tsx
+++ src/pages/base-page/base-page.tsx
@@ -1,7 +1,6 @@
 import { SideBar } from '@components/main-sidebar';
 import { Layout } from 'antd';
 import { Outlet } from 'react-router-dom';
-
 const BasePage = () => {
     return (
         <Layout>
--- src/pages/feed-backs/feed-backs.tsx
+++ src/pages/feed-backs/feed-backs.tsx
@@ -1,5 +1,8 @@
+import { Avatar, Comment, List, Rate, Tooltip } from 'antd';
 import Breadcrumbs from '@components/breadcrumb/breadcrumb';
-import './feed-backs.scss'
+import avatar from '../../../public/assets/icons/avatar.svg';
+import './feed-backs.scss';
+import { UserOutlined } from '@ant-design/icons';
 const routes = [
     {
         path: 'main',
@@ -10,12 +13,35 @@ const routes = [
     },
 ];
 
+
+
+
+
+
 const FeedBacks = () => {
     return (
         <div className='feed-backs-content'>
             <header>
                 <Breadcrumbs items={routes} />
             </header>
+            <div className='wrapper-card-commit'>
+                <div className='description-user'>
+                    <div><Avatar size={42} icon={<UserOutlined />} /></div>
+                    <div className='name-user'>
+                       <span> Вероника </span>
+                       <span> Киверова </span>
+                        </div>
+                </div>
+               <div className='wrapper-rate-text'>
+                    <div className='wrapper-rate-date'>
+                        <Rate value={3} />
+                        <div className='date-commit'>05.12.2023</div>
+                    </div>
+                    <p>
+                        Я очень довольна этим приложением! Оно помогает мне следить за своим здоровьем и физической формой, предлагая разнообразные упражнения и питание. Я люблю, что приложение адаптируется к моему уровню и целям, и дает мне полезные советы и обратную связь. Я рекомендую это приложение всем, кто хочет улучшить свою жизнь!
+                        </p>
+               </div>
+            </div>
         </div>
     );
 };
