d5f5b72995fec87689ee33e67814fe534341b037
Anton Belski <anton.belski@gmail.com>
2024-03-28 14:23:41 +0300

    feat: add fedbacks to component settings

--- src/components/settings-profile/settings-profile.tsx
+++ src/components/settings-profile/settings-profile.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { ArrowLeftOutlined, CheckOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
+import ModalWrite from '@components/popup/modal-write/modal-write';
 import SidebarTariff from '@components/sidebar-tariff/sidebar-tariff';
+import { PATHS } from '@constants/index';
 import free from '@public/icons/free.svg';
 import pro from '@public/icons/pro.svg';
 import { history } from '@redux/configure-store';
@@ -11,11 +13,28 @@ import './settings-profile.scss';
 
 const SettingsProfile = () => {
     const [isOpenTariff, setOpenTariff] = useState(false);
-
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const [switchSize, setSwitchSize] = useState<SwitchSize | undefined>('default');
     const [tooltipPlacement, setTooltipPlacement] = useState<
         'bottomLeft' | 'bottomRight' | 'topLeft' | 'topRight'
     >('bottomLeft');
+
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
+    
+    const handleShowErorModulSave = () => {
+        setIsModalOpen(false);
+    };
 
     useEffect(() => {
         const handleResize = () => {
@@ -126,13 +145,22 @@ const SettingsProfile = () => {
                             </div>
                         </div>
                         <div className='wrapper-bnt'>
-                            <Button>Написать отзыв </Button>
-                            <Button>Смотреть все отзывы </Button>
+                            <Button onClick={showModal}>Написать отзыв </Button>
+                            <Button onClick={() => history.push(PATHS.FEEDBACKS)}>
+                                Смотреть все отзывы{' '}
+                            </Button>
                         </div>
                     </div>
                 </div>
             </div>
-            <SidebarTariff open={isOpenTariff} onClose={closeTariff}  />
+            <SidebarTariff open={isOpenTariff} onClose={closeTariff} />
+            <ModalWrite
+                isOpen={isModalOpen}
+                onOk={handleOk}
+                onCancel={handleCancel}
+                centered={true}
+                onError={handleShowErorModulSave}
+            />
         </React.Fragment>
     );
 };
